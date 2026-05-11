51efc7290854c97403612e5386f0e704b2395d73
suolk <1548712503@qq.com>
2026-05-12 07:56:16 +0800

    update singbox service override

--- Command/confSingbox.sh
+++ Command/confSingbox.sh
@@ -24,8 +24,24 @@ cat > "$CONFIG_PATH" <<EOF
 }
 EOF
 
-if ! systemctl cat sing-box | grep -q -- "-c $CONFIG_PATH"; then
-    singbox_bin=$(command -v sing-box)
+singbox_bin=$(command -v sing-box)
+if ! systemctl cat sing-box >/dev/null 2>&1; then
+    cat > /etc/systemd/system/sing-box.service <<EOF
+[Unit]
+Description=sing-box service
+After=network.target nss-lookup.target
+
+[Service]
+ExecStart=$singbox_bin run -c $CONFIG_PATH
+Restart=on-failure
+RestartSec=10s
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    systemctl daemon-reload
+elif ! systemctl cat sing-box | grep -q -- "-c $CONFIG_PATH"; then
     mkdir -p /etc/systemd/system/sing-box.service.d
     cat > /etc/systemd/system/sing-box.service.d/override.conf <<EOF
 [Service]
